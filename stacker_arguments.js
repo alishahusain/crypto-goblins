64076a292066cc77b98e3bb6ef69747020822da0
Alisha Husain <43160608+alishahusain@users.noreply.github.com>
2022-09-30 09:16:24 -0700

    Update stacker_arguments.js

--- stacker_arguments.js
+++ stacker_arguments.js
@@ -1,7 +1,7 @@
 const deployments = require('./data/deployments');
 module.exports = [
     "StackedPupz",
-    "STACk",
+    "STACK",
     deployments.stackedPupzMainnet,
     "ipfs://QmWf3ywafrdzWx6QjUJiRe6NqMkb28rfPj3oBBkokTL199/"
 ]
